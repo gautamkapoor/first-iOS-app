b51ab353200bf1f63fa214a526f3ce8d2019d7c9
Gautam Kapoor <legautam.17@gmail.com>
2019-04-19 11:48:49 +0530

    First commit

---
+++ Hello_world/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hello_world
+//
+//  Created by Apple on 19/04/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
